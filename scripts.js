723b361a5f91c585e561e49b9eea77fce1291b67
taylir <tdl9716@gmail.com>
2023-02-28 17:17:58 -0500

    Finished the card JS and css now will be adding images and such

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Below Code is for the gradient grid
+
 const wrapper = document.getElementById("tiles");
 const bodyWrapper = document.querySelector(".tiles__wrapper");
 
@@ -64,3 +66,47 @@ window.onresize = () => {
   createGrid();
   goodGridSpeed();
 };
+
+//Above code is for the Gradient Grid
+
+//Below Code is for the card actions
+
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+function handleRight() {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "after";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "becoming-active-from-before";
+  }, 0);
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  }, 100);
+}
+
+function handleLeft() {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "before";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "becoming-active-from-after";
+  }, 0);
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  }, 100);
+}
